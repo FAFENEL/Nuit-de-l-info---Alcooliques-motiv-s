9f48ddb146a714da95d9f2c42137040dd9397b36
Jeremie-PARANT <parantjeremie@gmail.com>
2023-12-08 05:24:21 +0100

    Q1 Fini

---
+++ js.js
@@ -0,0 +1,33 @@
+function hauteur() {
+    var div_princ = document.getElementById("div_princ");
+    
+    if (div_princ.style.height === "600px") {
+        // Si la hauteur est déjà de 600px, réduire à 100px
+        div_princ.style.height = "100px";
+    } else {
+        // Sinon, agrandir à 600px
+        div_princ.style.height = "600px";
+    }
+}
+function hauteur2() {
+    var div_princ = document.getElementById("div_princ2");
+    
+    if (div_princ.style.height === "600px") {
+        // Si la hauteur est déjà de 600px, réduire à 100px
+        div_princ.style.height = "100px";
+    } else {
+        // Sinon, agrandir à 600px
+        div_princ.style.height = "600px";
+    }
+}
+function hauteur3() {
+    var div_princ = document.getElementById("div_princ3");
+    
+    if (div_princ.style.height === "600px") {
+        // Si la hauteur est déjà de 600px, réduire à 100px
+        div_princ.style.height = "100px";
+    } else {
+        // Sinon, agrandir à 600px
+        div_princ.style.height = "600px";
+    }
+}
